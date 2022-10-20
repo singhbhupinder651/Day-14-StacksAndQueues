e3835517ea18be59dc8f7999cc77567bfe2252ba
singhbhupinder651 <71844883+singhbhupinder651@users.noreply.github.com>
2022-10-20 12:26:46 +0530

    updated

---
+++ Stack.cs
@@ -0,0 +1,75 @@
+﻿using StackUsingLinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    internal class Stack
+    {
+
+        private Node top;
+
+        public Stack()
+        {
+            this.top = null;
+        }
+
+        
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+
+
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+        internal void Peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top of the stack ", this.top.data);
+        }
+
+        internal void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty, Deletion is not possible");
+                return;
+            }
+            Console.WriteLine("Value popped is {0} ", this.top.data);
+            this.top = this.top.next;
+        }
+
+        internal void IsEmpty()
+        {
+            while (this.top != null)
+            {
+                Peek();
+                Pop();
+            }
+            
+        }
+
+    }
+}
