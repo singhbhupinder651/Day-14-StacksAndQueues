834867b541d689173e8ae2254a59aec1b89452be
singhbhupinder651 <71844883+singhbhupinder651@users.noreply.github.com>
2022-10-20 12:24:41 +0530

    updated

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace StacksAndQueues
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Stacks and Queues Problem");
+            Console.WriteLine("Use case #1: Create a Stack");
+
+            Console.Write("Please select a program to run from options above: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+
+            switch(choice)
+            {
+                case 1:
+                    Stack stack = new Stack();
+                    stack.Push(70);
+                    stack.Push(30);
+                    stack.Push(56);
+                    stack.Display();
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid number from given options");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,46 @@
+﻿using StackUsingLinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    internal class Stack
+    {
+
+        private Node top;
+
+        public Stack()
+        {
+            this.top = null;
+        }
+
+        
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack ", value);
+        }
+
+
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+        
+
+    }
+}
